e5421a40f14660668620784c190d6babe34a4073
yaweiwang <xdjm562@qq.com>
2024-06-07 10:38:42 +0800

    update docker

---
+++ docker/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# echo "Please enter ARC password to login the registry"
+# docker login -u=mdude@aliyun.com registry.cn-hangzhou.aliyuncs.com
+
+# docker compose pull
+
+# docker compose build --no-cache --force-rm
+
+docker-compose down
+
+docker-compose up -d
