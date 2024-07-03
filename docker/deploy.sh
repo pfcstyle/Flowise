0e969bff4e6f6ed89e95324e4918f0deaacefbcb
yaweiwang <xdjm562@qq.com>
2024-07-03 17:39:06 +0800

    update docker

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-# echo "Please enter ARC password to login the registry"
-# docker login -u=mdude@aliyun.com registry.cn-hangzhou.aliyuncs.com
+echo "Please enter ARC password to login the registry"
+docker login -u=mdude@aliyun.com registry.cn-hangzhou.aliyuncs.com
 
 # docker compose pull
 
 # docker compose build --no-cache --force-rm
 
-docker-compose build
+docker-compose build && docker push registry.cn-hangzhou.aliyuncs.com/myelin2021/flowise-arcgis:latest
+
+# docker-compose pull
 
 docker-compose down
 
